6195d816fb0cc4a24580d4bc8b034fbf53c8da33
Joe Huss <detain@interserver.net>
2017-06-24 05:25:00 -0400

    removing 2 unused functions

--- src/Plugin.php
+++ src/Plugin.php
@@ -21,19 +21,6 @@ class Plugin {
 		];
 	}
 
-	public static function Install(GenericEvent $event) {
-		$plugin = $event->getSubject();
-		$serviceCategory = $plugin->add_service_category('licenses', 'abuse', 'Abuse');
-		$plugin->define('SERVICE_TYPES_ABUSE', $serviceCategory);
-		$serviceType = $plugin->add_service_type($serviceCategory, 'licenses', 'Abuse');
-		$plugin->add_service($serviceCategory, $serviceType, 'licenses', 'Abuse License', 10.00, 0, 1, 1, '');
-		$plugin->add_service($serviceCategory, $serviceType, 'licenses', 'Abuse Type2 License', 11.95, 0, 1, 2, '');
-		$plugin->add_service($serviceCategory, $serviceType, 'licenses', 'KernelCare License', 2.95, 0, 1, 16, '');
-	}
-
-	public static function Uninstall(GenericEvent $event) {
-	}
-
 	public static function Menu(GenericEvent $event) {
 		// will be executed when the licenses.settings event is dispatched
 		$menu = $event->getSubject();
