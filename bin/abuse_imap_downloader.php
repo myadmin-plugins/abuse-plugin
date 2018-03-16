b7501b34c3444b54afe62a497de8103471dbf049
Joe Huss <detain@interserver.net>
2018-03-15 22:39:58 -0400

    updated copyright tags

--- bin/abuse_imap_downloader.php
+++ bin/abuse_imap_downloader.php
@@ -6,7 +6,7 @@
  * through its ip, we contact the clients letting them know of the abuse complaint.
  *
  * @author Joe Huss <detain@interserver.net>
- * @copyright 2017
+ * @copyright 2018
  * @package Abuse
  * @category Crontab
  */
