71b574c4fc53fb0392bfa846f26767e3398dc981
Joe Huss <detain@interserver.net>
2026-05-19 12:44:41 -0400

    updates to cli scripts fixing ima sesison handling

--- bin/abuse_imap_downloader.php
+++ bin/abuse_imap_downloader.php
@@ -13,7 +13,10 @@
 
 include_once __DIR__.'/../../../../include/functions.inc.php';
 include_once __DIR__.'/../src/ImapAbuseCheck.php';
-\MyAdmin\App::session()->create(160307, 'services', false, 0, false, substr(basename($_SERVER['argv'][0], '.php'), 0, 32));
+\MyAdmin\App::session()->sessionid = substr(basename($_SERVER['argv'][0], '.php'), 0, 32);
+\MyAdmin\App::session()->account_id = 160307;
+\MyAdmin\App::session()->appnocache('ima', 'services');
+\MyAdmin\App::tf()->ima = 'services';
 $db = \MyAdmin\App::db();
 $db->query('select abuse_ip,count(abuse_lid) as count from abuse where Year(abuse_time)=Year(now()) and Month(abuse_time)=Month(now()) and Day(abuse_time)=Day(now()) group by abuse_ip');
 $abuse_ips = [];
